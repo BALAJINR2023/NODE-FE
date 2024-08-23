2f00e103dee554517627870a0e8a80eb5d57e0ea
BALAJINR2023 <ravibalajinr@gmail.com>
2024-08-23 18:25:59 +0530

    nill

--- routes/auth/register.js
+++ routes/auth/register.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import { userModel } from '../../db.utils/models.js';
-// import { mailOptions, transporter } from './mail-utils.js';
+import { mailOptions, transporter } from './mail-utils.js';
 const registerRouter= express.Router();
 
 registerRouter.post('/', async(req, res) => {
 const userData=req.body;
+// console.log(userData);
     const userObj= await userModel.findOne({email: userData.email});
     if (userObj) {
         res.status(400).send({ msg: "User already exists" });
@@ -21,10 +22,10 @@ const userData=req.body;
             id, });
 
             await newUser.save();
-            // await transporter.sendMail({
-            //   ...mailOptions,
-            //   to: userData.email,                         
-            // });
+            await transporter.sendMail({
+              ...mailOptions,
+              to: userData.email,                         
+            });
             res.send({ msg: "User saved successfully" });}
         });
     } 
